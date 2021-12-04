cb225563f5dfcbbe4c8f069a959b8880efa3045c
GishaJamesY <gisha@yajee.com>
2021-12-04 15:56:02 +0800

    2021 Day-1

---
+++ day-1.ex
@@ -0,0 +1,33 @@
+defmodule Day1 do
+  def get_result() do
+    stream = File.stream!("sample.txt")
+
+    contents =
+      stream
+      |> Enum.map(fn x ->
+        x
+        |> String.trim()
+        |> String.to_integer()
+      end)
+
+    [prev | next] = contents
+
+    init = %{
+      count: 0,
+      prev: prev
+    }
+
+    next
+    |> Enum.reduce(init, fn x, acc ->
+      acc =
+        if x > acc.prev do
+          Map.put(acc, :count, acc.count + 1)
+        else
+          acc
+        end
+
+      Map.put(acc, :prev, x)
+    end)
+    |> Map.get(:count)
+  end
+end
